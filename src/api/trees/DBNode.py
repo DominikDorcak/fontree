9efa5abe0060d9d86f6390f90baffd222edbc4ed
Domnik Dorčák <dominik.dorcak@gmail.com>
2022-04-14 17:22:06 +0200

    priprava ukladania nodes do DB part 2

--- src/api/trees/DBNode.py
+++ src/api/trees/DBNode.py
@@ -1,3 +1,6 @@
+from src import database
+
+
 class DBNode:
     def __init__(self, id, left_child, right_child, feature_id, class_id):
         self.id = id
@@ -8,6 +11,48 @@ class DBNode:
 
     def is_leaf(self):
         return self.left_child == self.right_child
+
+    def saveRadix(self):
+        cnx = database.getDbConnection()
+        cursor = cnx.cursor()
+        sql = "INSERT INTO node () VALUES () "
+        cursor.execute(sql)
+        self.id = int(cursor.lastrowid)
+        self.addRadixId(self.id)
+        self.update()
+        return self.id
+
+    def addRadixId(self, radix_id):
+        self.right_child += radix_id
+        self.left_child += radix_id
+
+    def update(self):
+        cnx = database.getDbConnection()
+        cursor = cnx.cursor()
+        sql = "UPDATE node SET left_child_id=%s, " \
+              "right_child_id=%s, " \
+              "question_id=%s, " \
+              "font_id=%s " \
+              "WHERE node_id=%s "
+        data = (self.left_child,
+                self.right_child,
+                self.feature_id,
+                self.class_id,
+                self.id)
+        cursor.execute(sql, data)
+
+    def saveNode(self,radix_id):
+        cnx = database.getDbConnection()
+        self.addRadixId(radix_id)
+        cursor = cnx.cursor()
+        sql = "INSERT INTO  node (left_child_id,right_child_id,question_id,font_id)" \
+              " VALUES " \
+              "(%integer ,%integer, %integer, %integer )"
+        data = (self.left_child,
+                self.right_child,
+                self.feature_id,
+                self.class_id,)
+        cursor.execute(sql, data)
 
     def toString(self):
         res = 'NODE:\n' \
--- src/trees.py
+++ src/trees.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 from sklearn import metrics
@@ -13,17 +12,14 @@ from src.api.trees.DBNode import DBNode
 def createTreeClasifier(dataArr):
     data = pd.DataFrame(dataArr)
     Y = data[0]
-    X = data[range(1,22)]
+    X = data[range(1, 22)]
     clf = DecisionTreeClassifier()
-    clf.fit(X,Y)
+    clf.fit(X, Y)
     pred = clf.predict(X)
 
-    print("Accuracy:", metrics.accuracy_score(Y,pred))
-    # drawGraph(clf,Y)
-    nodearray = nodesToArray(clf.tree_)
-    for n in nodearray:
-        print(n.toString())
-
+    print("Accuracy:", metrics.accuracy_score(Y, pred))
+    drawGraph(clf,Y)
+    saveTreeToDB(clf,Y)
 
 
 def loadFontMap(filePath):
@@ -34,7 +30,7 @@ def loadFontMap(filePath):
     for l in lines:
         space = l.find(' ')
         id = int(l[:space])
-        name = l[space+1:len(l)-1]
+        name = l[space + 1:len(l) - 1]
         d[id] = name
     return d
 
@@ -62,37 +58,43 @@ def drawFromCSVSmall(filename):
     pred = clf.predict(X.head(10))
 
     print("Accuracy:", metrics.accuracy_score(Y.head(10), pred))
-    drawGraph(clf, Y,"smallTree.png")
+    drawGraph(clf, Y, "smallTree.png")
 
 
 def drawGraph(clf, Y, imageName="tree.png"):
-    dot_data = open("trees.dot","w+")
+    dot_data = open("trees.dot", "w+")
     fontmap = loadFontMap('data/fonts.txt')
-    export_graphviz(clf, out_file=dot_data,
+    export_graphviz(clf,
+                    out_file=dot_data,
                     filled=True, rounded=True,
-                    special_characters=True, class_names=[fontmap[x] for x in Y])
+                    special_characters=True,
+                    class_names=[fontmap[x] for x in Y])
     dot_data.close()
     text_data = open('texttree.txt', 'w+')
     text_data.write(export_text(clf))
     text_data.close()
-    dot_data = open("trees.dot","r")
+    dot_data = open("trees.dot", "r")
     graph = pydotplus.graph_from_dot_data(dot_data.read())
     graph.write_png(imageName)
     Image(graph.create_png())
 
 
-
-
-def saveTreeToDB(clf):
+def saveTreeToDB(clf,Y):
     tree = clf.tree_
+    nodes = nodesToArray(tree,Y)
+    radix_id = nodes[0].saveRadix()
+    for i in range(1,len(nodes)):
+        nodes[i].saveNode(radix_id)
 
 
-def nodesToArray(tree):
+
+def nodesToArray(tree,Y):
     leftch = tree.children_left
     rightch = tree.children_right
     feature = tree.feature
     classes = tree.value
     nodes = []
     for i in range(len(leftch)):
-        nodes.append(DBNode(i,leftch[i],rightch[i],feature[i],np.argmax(classes[i])))
+        classid = np.argmax(classes[i])
+        nodes.append(DBNode(i, leftch[i], rightch[i], feature[i]+1 if feature[i] >= 0 else None, Y[classid]))
     return nodes
