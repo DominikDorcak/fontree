61aa8cc77f450f63545482b5e34fde5d1b387051
Domnik Dorčák <dominik.dorcak@gmail.com>
2022-04-22 19:54:24 +0200

    pripravene loadovanie otazok

---
+++ src/api/Status.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from flask_restful import Resource, abort
+from src import database
+
+
+class Status(Resource):
+
+    def get(self):
+        db = database.getDbConnection()
+        c = db.cursor()
+        online = True
+        json = {}
+        try:
+            starttime = datetime.now()
+            c.execute('SELECT version()')
+            res = c.fetchone()
+            time = datetime.now() - starttime
+        except Exception:
+            online = False
+
+        db_status = ("online: " + res[0] + " ping:" + str(time.total_seconds() * 1000) + "ms") if online else "offline"
+        json['db'] = {"online":online,
+                      "ping": str(time.total_seconds() * 1000) + "ms" if online else None}
+        return json, 200
--- src/api/server.py
+++ src/api/server.py
@@ -1,15 +1,15 @@
-from datetime import datetime
 
 import yaml
 from flask import Flask
 from flask_restful import Api
 
 
-import src.database as mariadb
 
 from src.api.Font import Font
 from src.api.Image import Image
 from src.api.Node import Node
+from src.api.Question import Question
+from src.api.Status import Status
 
 try:
     import importlib.resources as pkg_resources
@@ -21,37 +21,16 @@ from src import config
 
 app = Flask(__name__)
 cfg = yaml.safe_load(pkg_resources.read_text(config, 'config.yaml'))
-db = mariadb.getDbConnection()
 
 api = Api(app)
+api.add_resource(Status, '/')
 api.add_resource(Image, '/image/<path:path>')
 api.add_resource(Node, '/node/<int:id>')
 api.add_resource(Font, '/font/<int:id>')
+api.add_resource(Question, '/question/<int:id>')
 
 
 
-@app.route("/")
 
-def status():
-    c = db.cursor()
-    online = True
-    try:
-        starttime = datetime.now()
-        c.execute('SELECT version()')
-        res = c.fetchone()
-        time = datetime.now()-starttime
-    except Exception:
-        online = False
-
-    db_status = ("online: " + res[0] + " ping:" + str(time.total_seconds() * 1000) + "ms") if online else "offline"
-    return """<!DOCTYPE html>
-        <html>
-            <head>
-                <title>Status</title>
-            </head>
-        <body> 
-        <H2> API ONLINE</H2>
-        <p> Database: """ + db_status + """</body>
-        </html>"""
 
 
